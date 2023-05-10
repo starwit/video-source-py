ce1a00af6d57c0e3d85bde75e108c1f2992802c0
flonix8 <flstanek@googlemail.com>
2023-05-10 11:51:27 +0200

    Finish rough subprocess implementation (should work now)

--- video_source_py/videosource.py
+++ video_source_py/videosource.py
@@ -1,34 +1,44 @@
 import multiprocessing as mp
 import queue
 import time
-from ctypes import c_bool
 
 import cv2
 
 from .config import VideoSourceConfig
+from .errors import *
 
 
 class VideoSource:
-    def __init__(self, config: VideoSourceConfig, frame_queue: mp.Queue) -> None:
+    def __init__(self, config: VideoSourceConfig) -> None:
         if config.__class__.__name__ != 'VideoSourceConfig':
             raise TypeError('Config must be of type VideoSourceConfig')
         self.config = config
-        self.running = mp.Value(c_bool, True)
-        self.video_loop = _VideoLoop(self.running, frame_queue, config.uri)
+        self.stop_event = mp.Event()
+        self.frame_queue = mp.Queue(5)
+        self.video_loop = _VideoLoop(self.stop_event, self.frame_queue, config.uri)
 
     def start(self):
         self.video_loop.start()
 
     def stop(self):
-        self.running.value = False
-        # print('Calling join ', mp.current_process().pid)
-        # self.video_loop.join()
-        # print('Called join')
+        self.stop_event.set()
+    
+    def get_frame(self, block=True, timeout=10):
+        '''
+            Raises NoFrameError if no frame is available (after having waited for timeout if block is True).
+            Must not be called after .stop().
+        '''
+        if self.stop_event.is_set():
+            raise ClosedError('VideoSource has already been closed')
+        try:
+            return self.frame_queue.get(block, timeout)
+        except queue.Empty:
+            raise NoFrameError(f'No frame has been received after waiting for {timeout}s')
 
 class _VideoLoop(mp.Process):
-    def __init__(self, running: mp.Value, frame_queue: mp.Queue, uri: str, *args, **kwargs):
+    def __init__(self, stop_event, frame_queue: mp.Queue, uri: str, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.running = running
+        self.stop_event = stop_event
         self.frame_queue = frame_queue
         self.uri = uri
 
@@ -37,7 +47,7 @@ class _VideoLoop(mp.Process):
         if not cap.isOpened():
             raise IOError(f'Cannot open video source at {self.uri}')
 
-        while self.running.value:
+        while not self.stop_event.is_set():
             frame_ok, frame = cap.read()
             if not frame_ok:
                 break
@@ -48,4 +58,20 @@ class _VideoLoop(mp.Process):
                 time.sleep(0.01)
 
         cap.release()
-        # print(mp.current_process().pid)
+
+        # The queue must be drained before finishing, otherwise the queue feeder thread will not exit
+        self._drain_queue()
+
+    def _drain_queue(self):
+        # Remove one element to make room for the sentinel element (in case the queue is full)
+        try:
+            self.frame_queue.get_nowait()
+        except:
+            pass
+
+        # Put the sentinel (the explicit end) into the queue
+        self.frame_queue.put(None, block=True)
+
+        # Now we can be sure that the queue is empty if we remove all elements until we see the sentinel
+        while self.frame_queue.get(block=True) is not None:
+            pass
