2803eaf9c039171585a5a5a965573a4ac3cefa7c
flonix8 <flstanek@googlemail.com>
2024-06-19 11:36:21 +0200

    Version 1.0.1: Add VIDEO_SOURCE_PARALLELISM env var (limiting thread creation for less overhead; default 1)

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+import os
+PARALLELSIM = int(os.environ.get("VIDEO_SOURCE_PARALLELISM", 1))
+print(f'Parallelism set to {PARALLELSIM}')
+
+# Prevent numpy from using multiple threads, because we do not need to parallelize the computation and it created a lot of overhead
+# This needs to be done before any app module is imported!
+os.environ["OMP_NUM_THREADS"] = f'{PARALLELSIM}'
+os.environ["OPENBLAS_NUM_THREADS"] = f'{PARALLELSIM}'
+os.environ["MKL_NUM_THREADS"] = f'{PARALLELSIM}'
+os.environ["VECLIB_MAXIMUM_THREADS"] = f'{PARALLELSIM}'
+os.environ["NUMEXPR_NUM_THREADS"] = f'{PARALLELSIM}'
+
+# Limit OpenCV and subcomponents to one thread (again, order is important)
+os.environ["OPENCV_FFMPEG_THREADS"] = f'{PARALLELSIM}'
+import cv2
+cv2.setNumThreads(PARALLELSIM)
+
 from video_source_py import run_stage
 
 if __name__ == '__main__':
