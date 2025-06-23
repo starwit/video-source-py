ebc5a85afefd54e40f08b7971b75496c6ccca831
ztarbug <markus.zarbock@starwit.de>
2025-06-23 13:41:48 +0200

    add GPG signingus

---
+++ start_make.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo $0
+export PATH=/root/.local/bin/:$PATH
+cd /code
+echo "export GPG_KEY=${GPG_KEY}" > env.sh
+echo "export PASSPHRASE=${GPG_KEY}" >> env.sh
+cat env.sh
+source ./env.sh
+make
