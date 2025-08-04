25b5d8302943003a251dfa76116080d288c310d5
ztarbug <markus.zarbock@starwit.de>
2025-08-04 15:54:08 +0200

    add config to template

--- video_source_py/videosource.py
+++ video_source_py/videosource.py
@@ -129,7 +129,6 @@ class VideoSource:
             cv2.subtract(src1=frame, src2=frame, dst=frame, mask=self._mask)
             
     def _get_location(self, frame) -> VideoFrame:
-        # read position data and parse it into PositionMessage
         streamPositionMessage = self._redis_client.xrevrange('positionsource:self', count=1)
         positionMessage = PositionMessage()
         if len(streamPositionMessage) > 0:
